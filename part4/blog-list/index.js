c1d988f2ceaa7f8f5f6a6822a7fc9171dccd01b7
Raya <mail.raya15@gmail.com>
2022-07-16 16:22:49 +0700

    create utils and middleware

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -7,18 +7,31 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+const logger = require("./utils/logger");
+const config = require("./utils/config");
+const middleware = require("./utils/middleware");
+const blogsRouter = require("./controllers/blogs");
 
-const Blog = require("./models/blog");
+logger.info("connecting to", config.MONGODB_URI);
 
-const mongoUrl = process.env.MONGODB_URI;
-mongoose.connect(mongoUrl);
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDB:", error.message);
+  });
 
 app.use(cors());
 app.use(express.json());
+app.use(middleware.requestLogger);
 
-app.use("/api/blogs", require("./controllers/blogs"));
+app.use("/api/blogs", blogsRouter);
 
-const PORT = process.env.PORT || 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+app.listen(config.PORT, () =>
+  logger.info(`Server running on port ${config.PORT}`)
+);
