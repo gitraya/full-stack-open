19aa3ce2efeee7fb485e4eae8116d52d3f201b54
Raya <mail.raya15@gmail.com>
2022-07-16 16:25:46 +0700

    add solution for exercise 4.2

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,37 +1,10 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
-
+const app = require("./app");
 const http = require("http");
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
-const logger = require("./utils/logger");
 const config = require("./utils/config");
-const middleware = require("./utils/middleware");
-const blogsRouter = require("./controllers/blogs");
+const logger = require("./utils/logger");
 
-logger.info("connecting to", config.MONGODB_URI);
+const server = http.createServer(app);
 
-mongoose
-  .connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info("connected to MongoDB");
-  })
-  .catch((error) => {
-    logger.error("error connecting to MongoDB:", error.message);
-  });
-
-app.use(cors());
-app.use(express.json());
-app.use(middleware.requestLogger);
-
-app.use("/api/blogs", blogsRouter);
-
-app.use(middleware.unknownEndpoint);
-app.use(middleware.errorHandler);
-
-app.listen(config.PORT, () =>
-  logger.info(`Server running on port ${config.PORT}`)
-);
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
+});
