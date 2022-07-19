efea71a398870f82e7fed3b3757c0e3c788113df
Raya <mail.raya15@gmail.com>
2022-07-19 17:31:58 +0700

    add solution for exercise 4.13

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -90,4 +90,24 @@ test("a blog without title and url is not added", async () => {
   await api.post("/api/blogs").send(newBlog).expect(400);
 });
 
+describe("deletion of a blog", () => {
+  test("succeeds with status code 204 if id is valid", async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
+
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+    const blogsAtEnd = await helper.blogsInDb();
+
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
+
+    const titles = blogsAtEnd.map((n) => n.title);
+    expect(titles).not.toContain(blogToDelete.title);
+  });
+
+  test("fails with status code 400 if id is invalid", async () => {
+    await api.delete(`/api/blogs/1`).expect(400);
+  });
+});
+
 afterAll(() => mongoose.connection.close());
