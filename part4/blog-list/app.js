cf451b5e2125a14212a3276c3fc3208199565ceb
Raya <mail.raya15@gmail.com>
2022-08-04 20:28:34 +0700

    add solution for exercise 4.15

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -3,6 +3,7 @@ require("express-async-errors");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
+const usersRouter = require("./controllers/users");
 const blogsRouter = require("./controllers/blogs");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
@@ -23,6 +24,7 @@ app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use("/api/users", usersRouter);
 app.use("/api/blogs", blogsRouter);
 
 app.use(middleware.unknownEndpoint);
