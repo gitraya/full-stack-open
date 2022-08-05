6c51643dda00966fa378cf5bb2d1570ea2a17c0e
Raya <mail.raya15@gmail.com>
2022-08-05 22:25:36 +0700

    add solution for exercise 4.6, 4.7

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -20,4 +20,36 @@ const favoriteBlog = (blogs) => {
   );
 };
 
-module.exports = { dummy, totalLikes, favoriteBlog };
+const mostBlogs = (blogs) => {
+  const authorBlogs = blogs.reduce((prev, curr) => {
+    prev[curr.author] = (prev[curr.author] || 0) + 1;
+    return prev;
+  }, {});
+
+  const mostBlogsAuthor = Object.keys(authorBlogs).reduce((prev, curr) => {
+    return authorBlogs[prev] > authorBlogs[curr] ? prev : curr;
+  }, "");
+
+  return {
+    author: mostBlogsAuthor,
+    blogs: authorBlogs[mostBlogsAuthor],
+  };
+};
+
+const mostLikes = (blogs) => {
+  const authorLikes = blogs.reduce((prev, curr) => {
+    prev[curr.author] = (prev[curr.author] || 0) + curr.likes;
+    return prev;
+  }, {});
+
+  const mostLikesAuthor = Object.keys(authorLikes).reduce((prev, curr) => {
+    return authorLikes[prev] > authorLikes[curr] ? prev : curr;
+  }, "");
+
+  return {
+    author: mostLikesAuthor,
+    likes: authorLikes[mostLikesAuthor],
+  };
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes };
